1e82b00d3d0c6e7b1e16e6d3959766f6644b1a82
Devwezardtech <ejemarmaloloyon007@gmail.com>
2026-08-02 16:35:53 +0800

    add new resume updated

--- script/downloadCV.js
+++ script/downloadCV.js
@@ -1,7 +1,7 @@
 function downloadCV() {
     const link = document.createElement("a");
-    link.href = "./resume/EJEMAR-MALOLOY-ON-RESUME.pdf";
-    link.download = "EJEMAR-MALOLOY-ON-RESUME.pdf";
+    link.href = "./resume/EJEMAR MALOLOY-ON _ RESUME.pdf";
+    link.download = "EJEMAR MALOLOY-ON _ RESUME.pdf";
 
     document.body.appendChild(link);
     link.click();
